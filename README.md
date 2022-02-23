4900865945e5e8e0ba36b3267ba608e76382b309
Joao Pansani <joaoaugustom.pansani@gmail.com>
2022-02-23 19:45:08 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1 align="center">
+    Proffy 
+</h1>
+
+<br>
+
+## 🚀 Tecnologies
+
+This project was developed with the following technologies:
+
+- [Node.js](https://nodejs.org/en/)
+- [Express](https://expressjs.com/)
+- [Nunjucks](https://mozilla.github.io/nunjucks/)
+- [SQLite](https://www.sqlite.org/index.html)
+
+
+## 💻 Project
+
+Proffy is a demo, front-end studying application, that helps people to find teachers online.
+
+## :memo: License
+
+This project is under the MIT license. See the file [LICENSE](LICENSE.md) for more details.
+
+---
+
+Made with ♥ by JoaoAugustoPansani :wave: 
---
+++ src/database/test.js
@@ -0,0 +1,62 @@
+const Database = require('./db');
+const createProffy = require('./createProffy')
+
+Database.then(async (db) => {
+    // Inserir dados
+    proffyValue = {
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "4353267547",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+    }
+
+    classValue = {
+        subject: 1,
+        cost: "40",
+        //o proffy_id vira pelo data base
+    }
+
+    classScheduleValues = [
+        {
+            weekday: 1,
+            time_from: 720,
+            time_to: 1220
+        },
+        {
+            weekday: 0,
+            time_from: 520,
+            time_to: 1220
+        }
+        //o class_id id vira pelo data base
+    ]
+
+
+    //Consultar dados inseridos
+
+    //Todos os Proffys
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    // console.log(selectedProffys)
+
+    //Consultar as classes de um determinado professor e trazer junto os dados
+    const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys 
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+        
+    `)
+    // console.log(selectClassesAndProffys)
+
+    //Criando filtros para as buscas de professor por horario
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = "1"
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "1300"
+        AND class_schedule.time_to > "1300"
+    `)
+    
+    // console.log(selectClassesSchedules)
+})
